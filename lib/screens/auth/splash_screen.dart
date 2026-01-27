d7d6967d90352542a0f4778db6bfaaa1060dc09b
Creamoka <aryamaulanayusuf467@gmail.com>
2026-01-28 00:45:05 +0700

    merge all brench

--- lib/screens/auth/splash_screen.dart
+++ lib/screens/auth/splash_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
-import 'dart:async';
->>>>>>> rizal
 import '../../routes/app_routes.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,7 +12,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-<<<<<<< HEAD
     _navigate();
   }
 
@@ -28,19 +23,6 @@ class _SplashScreenState extends State<SplashScreen> {
       context,
       AppRoutes.product, // langsung ke product dulu
     );
-=======
-    _navigateToHome();
-  }
-
-  Future<void> _navigateToHome() async {
-    // Tunggu 2 detik untuk menampilkan splash screen
-    await Future.delayed(const Duration(seconds: 2));
-
-    if (!mounted) return;
-
-    // Langsung arahkan ke home tanpa cek auth status
-    Navigator.pushReplacementNamed(context, AppRoutes.home);
->>>>>>> rizal
   }
 
   @override
@@ -48,23 +30,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-<<<<<<< HEAD
         child: Text(
           'MERCHVERSE',
           style: TextStyle(
             fontSize: 26,
             fontWeight: FontWeight.bold,
-=======
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 45),
-          child: const Text(
-            'MERCHVERSE',
-            style: TextStyle(
-              fontSize: 29,
-              fontFamily: 'Raleway',
-              color: Colors.black,
-            ),
->>>>>>> rizal
           ),
         ),
       ),
