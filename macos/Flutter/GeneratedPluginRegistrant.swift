7a801d086a794496d88a9122ce61901da155b1e6
Creamoka <aryamaulanayusuf467@gmail.com>
2026-01-28 00:38:46 +0700

    pull brench 'rizal'

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,7 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
-<<<<<<< HEAD
 import path_provider_foundation
-=======
->>>>>>> arya
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -20,9 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-<<<<<<< HEAD
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-=======
->>>>>>> arya
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
